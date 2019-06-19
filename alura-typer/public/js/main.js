df3b2f1dcafcac45fdf3f7423efa989f07042a47
guilherme.sabino <guilherme.sabino@icolabora.com>
2019-06-19 12:20:09 -0300

    commit

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,33 +1,39 @@
-// Salva tempo inicial pra retornar pra ele depois, e salva campo digitado
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-$(function () {
-    atualizaTmanahoFrase();
+$(function() {
+    atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
     inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
     atualizaPlacar();
+
+    $("#usuarios").selectize({
+        create: true,
+        sortField: 'text'
+    });
+
+    $(".tooltip").tooltipster({
+        trigger: "custom"
+    });
 });
 
-// Atualiza tamanho da frase 
-function atualizaTmanahoFrase() {
-    var frase = $(".frase").text();
-    var numeroPalavras = frase.split(" ").length;
-    var tamanhoFrase = $("#tamanho-frase");
-    tamanhoFrase.text(numeroPalavras);
-}
-
-// Atualiza tempo inicial
 function atualizaTempoInicial(tempo) {
     tempoInicial = tempo;
-    $('#tempo-digitacao').text(tempo);
+    $("#tempo-digitacao").text(tempo);
 }
 
-// Conta o numero de caracteres e palavras
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras  = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+
+    tamanhoFrase.text(numPalavras);
+}
+
 function inicializaContadores() {
-    campo.on("input", function () {
+    campo.on("input", function() {
         var conteudo = campo.val();
 
         var qtdPalavras = conteudo.split(/\S+/).length - 1;
@@ -38,35 +44,12 @@ function inicializaContadores() {
     });
 }
 
-// Decrescendo tempo
-function inicializaCronometro() {
-    campo.one("focus", function () {
-        var tempoRestante = $("#tempo-digitacao").text();
-        var cronometroId = setInterval(function () {
-            tempoRestante--;
-            $("#tempo-digitacao").text(tempoRestante);
-            if (tempoRestante < 1) {
-                clearInterval(cronometroId);
-                finalizaJogo();
-            }
-        }, 1000);
-    });
-}
-
-// O que fazer quando jogo finalziar
-function finalizaJogo() {
-    campo.attr("disabled", true);
-    campo.toggleClass("campo-desativado");
-    inserePlacar();
-}
-
-// Observando o que o cara escreve
 function inicializaMarcadores() {
-    campo.on("input", function () {
+    campo.on("input", function() {
         var frase = $(".frase").text();
         var digitado = campo.val();
-        var comparavel = frase.substr(0, digitado.lenght);
-        // Comparando o digitado com a frase
+        var comparavel = frase.substr(0, digitado.length);
+
         if (digitado == comparavel) {
             campo.addClass("borda-verde");
             campo.removeClass("borda-vermelha");
@@ -77,17 +60,34 @@ function inicializaMarcadores() {
     });
 }
 
-// Botão para reiniciar
-function reiniciaJogo() {
-    $("#botao-reiniciar").click(function () {
-        campo.attr("disabled", false);
-        campo.val("");
-        $("#contador-palavras").text("0");
-        $("#contador-caracteres").text("0");
-        $("#tempo-digitacao").text(tempoInicial);
-        inicializaCronometro();
-        campo.toggleClass("campo-desativado");
-        campo.removeClass("borda-verde");
-        campo.removeClass("borda-vermelha");
+function inicializaCronometro() {
+    campo.one("focus", function() {
+        var tempoRestante = $("#tempo-digitacao").text();
+    	var cronometroID = setInterval(function() {
+    		tempoRestante--;
+    		$("#tempo-digitacao").text(tempoRestante);
+    		if (tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+    		}
+    	}, 1000);
     });
+}
+
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
 }
--- alura-typer/public/js/placar.js
+++ alura-typer/public/js/placar.js
@@ -1,44 +1,40 @@
-// Chamando o placar através do botão
 $("#botao-placar").click(mostraPlacar);
 $("#botao-sync").click(sincronizaPlacar);
 
-// Coloca o placar na tabela
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Guilherme";
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
     linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.append(linha);
-
     $(".placar").slideDown(500);
     scrollPlacar();
 }
 
-// Função que desce a página até o placar 
 function scrollPlacar() {
     var posicaoPlacar = $(".placar").offset().top;
-
-    $("html, body").animate(
-        {
-            scrollTop: posicaoPlacar + "px"
-        }, 1000);
+    $("body").animate(
+    {
+        scrollTop: posicaoPlacar + "px"
+    }, 1000);
 }
 
-// Função de criação de linhas de pontuação
-function novaLinha(usuario, numPalavras) {
+function novaLinha(usuario, palavras) {
     var linha = $("<tr>");
     var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaPalavras = $("<td>").text(palavras);
     var colunaRemover = $("<td>");
 
     var link = $("<a>").addClass("botao-remover").attr("href", "#");
     var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
     link.append(icone);
+
     colunaRemover.append(link);
+
     linha.append(colunaUsuario);
     linha.append(colunaPalavras);
     linha.append(colunaRemover);
@@ -46,54 +42,61 @@ function novaLinha(usuario, numPalavras) {
     return linha;
 }
 
-// Função de remoção de linhas de pontuação
 function removeLinha() {
     event.preventDefault();
-    var linhaTab = $(this).parent().parent();
-    linhaTab.fadeOut(1000);
-    // Função para remover linha após 1 segundo, isso após esmaecer a linha
-    setTimeout(function () {
-        linhaTab.remove();
+    var linha = $(this).parent().parent();
+
+    linha.fadeOut(1000);
+    setTimeout(function() {
+        linha.remove();
     }, 1000);
 }
 
-// Função de mostrar o placar
 function mostraPlacar() {
-    $(".placar").stop().slideToggle(1200);
+    $(".placar").stop().slideToggle(600);
 }
 
-// Função de sincronizar placar e jogar na url placar
-function sincronizaPlacar() {
+function sincronizaPlacar(){
+
     var placar = [];
     var linhas = $("tbody>tr");
 
-    linhas.each(function () {
+    linhas.each(function(){
         var usuario = $(this).find("td:nth-child(1)").text();
-        var palavras = $(this).ind("td:nth-child(2)").text();
+        var palavras = $(this).find("td:nth-child(2)").text();
 
         var score = {
             usuario: usuario,
-            pontos: palavras
+            pontos: palavras            
         };
 
         placar.push(score);
-    });
 
-    var dados = {
-        placar: placar
-    };
+        var dados = {
+            placar: placar
+        };
 
-    $.post("http://localhost:3000/placar", dados, function () {
-        console.log("Log salvo no servidor!");
+        $.post("http://localhost:3000/placar", dados , function() {
+            console.log("Placar sincronizado com sucesso");
+            $(".tooltip").tooltipster("open"); 
+        }).fail(function(){
+            $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar"); 
+        }).always(function(){ //novo
+            setTimeout(function() {
+            $(".tooltip").tooltipster("close"); 
+        }, 1200);
+});
+
     });
 }
 
-// Função que atualiza placar quando entra na página
-function atualizaPlacar() {
-    $.get("http://localhost:3000/placar", function (data) {
-        $(data).each(function () {
+function atualizaPlacar(){
+    $.get("http://localhost:3000/placar",function(data){
+        $(data).each(function(){
             var linha = novaLinha(this.usuario, this.pontos);
+
             linha.find(".botao-remover").click(removeLinha);
+
             $("tbody").append(linha);
         });
     });
